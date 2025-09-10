69e2c7899cdba497111f7c9b502efcdb25d6f3fa
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-09-10 06:34:21 +0200

    functions

---
+++ python/squares.py
@@ -0,0 +1,10 @@
+from functions import square
+
+
+for i in range(10):
+    print(f"The Square of {i} is {square(i)}")
+
+for i in range(10):
+    print(f"The Square of {i} is {function.square(i)}")
+
+
