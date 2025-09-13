e44dc565f7b557374ff6ff0434b552019e54bef3
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-09-13 13:28:06 +0200

    exceptions

--- python/exceptions.py
+++ python/exceptions.py
@@ -1,7 +1,15 @@
-x = int(input("x: "))
-y = int(input("y: "))
+import sys
 
+try:
+    x = int(input("x: "))
+    y = int(input("y: "))
+except ValueError:
+    print("Error: invalid input.")
+    sys.exit(1)
 
-result = x / y
-
+try:
+    result = x / y
+except ZeroDivisionError:
+    print("Error: cannot divide by 0.")
+    sys.exit(1)
 print(f"{x} / {y} = {result}")
