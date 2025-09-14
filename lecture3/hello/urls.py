fdd088e8fb57f237a2361601bef66104790140a5
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-09-14 13:06:42 +0200

    hello, html

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path("",views.index,name ="index" )
+    path("",views.index, name ="index" ),
+    path("<str:name>",views.greet, name="greet"),
+    path("mbali",views.mbali, name="Mbali"),
+    path("kagisho",views.kagisho, name="kagisho"),
+    
+
 ]
--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,4 +3,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello,World")
+    return render(request,"hello/index.html")
+
+
+
+def mbali(request):
+    return HttpResponse("hello Mbali!")
+
+
+def kagisho(request):
+    return HttpResponse("hello Mbali's Boyfrend")
+
+
+def greet(request, name):
+    return render(request, "hello/greet.html",{
+        "name":name.capitalize()
+    })
