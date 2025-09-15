c0c71cde4da3d95052a23e03e4a0dd7c93f1712d
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-09-15 06:05:26 +0200

    task.session

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,11 +1,36 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-tasks = ["shop","buy","afford"]
+
+
+
+class NewTaskForm(forms.form):
+    task = forms.CharField(label= "New Task")
+    priority = forms.Integerfield(label= "priority",min_value =1,max_value=10)
+
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html",{
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request,"tasks/add.html",{
+                "form":form
+            })
+
+
+
+    return render(request, "tasks/add.html",{
+        "form":NewTaskForm()
+    })
