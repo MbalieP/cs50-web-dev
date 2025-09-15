4db09005e68ac010e24014426a5294ca7b106ca1
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-09-15 04:46:48 +0200

    task

--- lecture3/lecture3/settings.py
+++ lecture3/lecture3/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'hello',
     'newyear',
-    'tasks'
+    'tasks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
